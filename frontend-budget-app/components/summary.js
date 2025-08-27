0620e3df0057031072a73d204da02d490596ec7e
Andrew Vo <andrewvo1024@gmail.com>
2025-08-27 14:38:24 -0500

    minimal styling for the summary component done

--- frontend-budget-app/components/summary.js
+++ frontend-budget-app/components/summary.js
@@ -4,9 +4,9 @@ import { Summary } from "../mock-data/summary"
 export default function SummaryComponent() {
     return (
         <View style={styles.container}>
-            <Text>Budget Progress</Text>
+            <Text style={styles.title}>Budget Progress</Text>
             {Summary.map((sum) => (
-                <View key={sum.id}>
+                <View key={sum.id} style={styles.row}>
                     <Text>{sum.category} - ${sum.amount}</Text>
                 </View>
             ))}
@@ -16,6 +16,26 @@ export default function SummaryComponent() {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "blue"
-    }
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: 15,
+        margin: 10,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 20,
+        marginLeft: 15,
+    },
+    row: {
+        flexDirection: "row",
+        marginBottom: 10,
+        marginLeft: 15,
+        marginRight: 15,
+        //Styling for divider line
+        borderBottomWidth: 1,
+        borderBottomColor: "#eee",  
+        paddingBottom: 10,
+        // backgroundColor: "red"
+    },
 })
