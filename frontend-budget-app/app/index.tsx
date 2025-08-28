c7a4da61d59c4b87ff2d7d63a01d30c957b286dc
Andrew Vo <andrewvo1024@gmail.com>
2025-08-27 22:45:50 -0500

    took out header, styled dashboard title, implemented safeareaview

--- frontend-budget-app/app/index.tsx
+++ frontend-budget-app/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import TransactionComponent from "../components/transactions"
 import SummaryComponent from "../components/summary"
 import AddTransactionComponent from "../components/add_transaction_component"
@@ -6,13 +6,23 @@ import AddTransactionComponent from "../components/add_transaction_component"
 export default function Index() {
 
   return (
+    <SafeAreaView>
     <View>
       <View>
-        <Text>Budget App</Text>
+        <Text style={styles.title}>Budget App</Text>
         <TransactionComponent />
         <SummaryComponent />
         <AddTransactionComponent />
       </View>
     </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginLeft: 20,
+  }
+})
