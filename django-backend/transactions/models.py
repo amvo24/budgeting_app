ee54ac23b2b2c84fa57b67b87655ace61ce3e234
Andrew Vo <andrewvo1024@gmail.com>
2025-08-28 14:52:59 -0500

    model for transactions made

---
+++ django-backend/transactions/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Transaction(models.Model):
+    CATEGORY_CHOICES = [
+        ("groceries", "Groceries"),
+        ("bills", "Bills"),
+        ("utilities", "Utilities"),
+        ("rent/mortgage", "Rent/Mortgage"),
+        ("entertainment", "Entertainment"),
+        ("dining", "Dining"),
+        ("misc", "Misc"),   
+    ]
+
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=255)
+    date = models.DateField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = models.TextField(blank=True, null=True)
+    
+    #PAID BY WILL BE A FOREIGN KEY TO USER MODEL LATER ON
+    paid_by = models.CharField(max_length=100)     
+    
+    def __str__(self):
+        return f"{self.description} - {self.category} - {self.amount}"
