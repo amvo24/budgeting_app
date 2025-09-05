c3ed96609195cbfcfaf03351058dd2fc131d77cf
Andrew Vo <andrewvo1024@gmail.com>
2025-09-05 17:37:38 -0500

    added a summary viewset and added it to urls

--- django-backend/summaries/urls.py
+++ django-backend/summaries/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet
+from .views import CategoryViewSet, SummaryView
 
 router = DefaultRouter()
-router.register(r'categories',CategoryViewSet,  basename='categories')
+router.register(r'categories',CategoryViewSet, basename='categories')
+# router.register(r'summaries/monthly', SummaryView.as_view, basename='monthly-summary')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('summaries/monthly/', SummaryView.as_view(), name='monthly-summary'),
+]
+
+urlpatterns += router.urls
--- django-backend/summaries/views.py
+++ django-backend/summaries/views.py
@@ -1,7 +1,11 @@
+from django.db.models import Sum
+from django.utils.timezone import now
 from rest_framework import viewsets
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
+from transactions.models import Transaction
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -10,6 +14,24 @@ class CategoryViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-# class SummaryView(APIView):
-#     def get(self, request):
-#         queryset = 
+class SummaryView(APIView):
+    def get(self, request):
+        year = int(request.query_params.get("year", now().year))
+        month = int(request.query_params.get("month", now().month))
+
+        from calendar import monthrange
+        start_date = f"{year}-{month:02d}-01"
+        end_day = monthrange(year, month)[1]
+        end_date = f"{year}-{month:02d}-{end_day}"
+
+        transaction_summary = (
+            Transaction.objects.filter(
+                user=request.user,
+                created_at=[start_date, end_date]
+                )
+                .values("category_name")
+                .annotate(total_spent=Sum("amount"))
+                .order_by("-total_spent")
+            )
+        
+        return Response(transaction_summary)
